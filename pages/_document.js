20ac97f7a91881edc4a266d818c53316b659fa35
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-12-02 21:49:39 +0500

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/SquidFont.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
